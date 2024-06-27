55dbff563754195f9b3df0677ec8eb73ff4c05b8
GustavRugaard <98323375+GustavRugaard@users.noreply.github.com>
2024-06-27 11:26:05 +0200

    Tilføjelse af bois' pointsystem

--- TBTFEuro.py
+++ TBTFEuro.py
@@ -45,6 +45,8 @@ class TBTFEuro:
 
         self._ImporterGrupperResultater()
         self._BeregnGrupperStilling()
+        self._ImporterBud()
+        self._BeregnGrupperBois()
 
     def _ImporterGrupperResultater(self):
         # Importerer resultater fra gruppespillet
@@ -140,4 +142,84 @@ class TBTFEuro:
         html += "</div>"
 
         display(HTML(html))
+
+    def _ImporterBud(self):
+        # Importer bois bud på gruppespillet
+        BudFil = 'Bud.xlsx'
+
+        self.GrupperBud = pd.read_excel(
+            BudFil,
+            sheet_name=None,
+            header=None,
+            names=['Hold 1', 'Hold 2', 'Mål 1', 'Mål 2']
+        )
     
+        for boi, ark in self.GrupperBud.items():
+            ark = ark.dropna()
+            ark[['Mål 1', 'Mål 2']] = ark[['Mål 1', 'Mål 2']].astype(int)
+            ark = ark.to_dict(orient='records')
+            self.GrupperBud[boi] = ark
+
+    def _BeregnGrupperBois(self):
+        # Beregner point i gruppespillet for bois' bud
+        self.GrupperBoisPoint = {boi: [] for boi in self.GrupperBud.keys()}
+
+        for boi in self.GrupperBud.keys():
+            for kamp, værdi in enumerate(self.GrupperBud[boi]):
+                if kamp < len(self.GrupperResultater): # Tjekker kun bud så længe der er resultater
+                    bHold1 = værdi['Hold 1'] # Definerer hold i bud
+                    bHold2 = værdi['Hold 2']
+
+                    Hold1 = self.GrupperResultater[kamp]['Hold 1'] # Definerer hold i kamp
+                    Hold2 = self.GrupperResultater[kamp]['Hold 2']
+
+                    bMål1 = værdi['Mål 1'] # Definerer mål i bud
+                    bMål2 = værdi['Mål 2']
+
+                    Mål1 = self.GrupperResultater[kamp]['Mål 1'] # Definerer mål i kamp
+                    Mål2 = self.GrupperResultater[kamp]['Mål 2']
+
+                    if bMål1 > bMål2: # Definerer vinder i bud
+                        bVinder = bHold1
+                    elif bMål1 < bMål2:
+                        bVinder = bHold2
+                    else:
+                        bVinder = 'Uafgjort'
+
+                    if Mål1 > Mål2: # Definerer vinder i kamp
+                        Vinder = Hold1
+                    elif Mål1 < Mål2:
+                        Vinder = Hold2
+                    else:
+                        Vinder = 'Uafgjort'
+
+                    if [bHold1, bHold2] == [Hold1, Hold2]: # Tjekker at kamp og bud er på samme hold
+                        if [bMål1, bMål2] == [Mål1, Mål2]: # Regel 1: Resultat og begge målscorer
+                            bPoint = 7
+                        elif (bMål1 == Mål1 or bMål2 == Mål2) and bVinder == Vinder: # Regel 2: Resultat og én målscore
+                            bPoint = 4
+                        elif bVinder == Vinder: # Regel 3: Resultat
+                            bPoint = 3
+                        elif bMål1 == Mål1 or bMål2 == Mål2: # Regel 4: Én målscore
+                            bPoint = 1
+                        else:
+                            bPoint = 0
+                    else:
+                        bPoint = np.nan
+                    
+                    self.GrupperBoisPoint[boi].append(bPoint)
+
+        # Beregner samlet stilling for bois
+        PointSum = {boi: sum(filter(lambda x: x == x, bPoint)) for boi, bPoint in self.GrupperBoisPoint.items()}
+
+        self.BoisStilling = pd.DataFrame(list(PointSum.items()), columns=['Boi', 'Point'])
+        self.BoisStilling = self.BoisStilling.sort_values(by='Point', ascending=False).reset_index(drop=True)
+
+    def _VisBoisStilling(self):
+        # Viser bois stilling
+        html = f"""
+        <div style="display: flex; justify-content: center;">
+            {self.BoisStilling.to_html(index=False)}
+        </div>
+        """
+        display(HTML(html))
